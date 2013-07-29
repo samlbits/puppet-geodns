59adfe0c35cf881b20812ca7b90002141d7eef89
Leif Johansson <leifj@sunet.se>
2013-07-29 14:37:10 +0200

    import

---
+++ README.md
@@ -0,0 +1,4 @@
+puppet-geodns
+===========
+
+A puppet module for the geodns setup used by samlbits.net
---
+++ manifests/init.pp
@@ -0,0 +1,47 @@
+
+Exec {
+  path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+}
+
+include ufw
+
+class geodns {
+   package {'golang':
+      ensure => 'installed'
+   }
+   package {'build-essential':
+      ensure => 'installed'
+   }
+   package {'libgeoip-dev':
+      ensure => 'installed'
+   }
+   package {'git-core':
+      ensure => 'installed'
+   }
+   exec {'geodns-install':
+      require   => [Package[golang],Package[libgeoip-dev]],
+      command   => "go get github.com/abh/geodns",
+      creates   => "/usr/lib/go/src/pkg/github.com/abh/geodns"
+   }
+   file {'geodns-upstart':
+      path      => '/etc/init/geodns.conf',
+      ensure    => file,
+      require   => Exec['geodns-install'],
+      content   => template('geodns/geodns-upstart.erb')
+   }
+   file {'geodns-defaults':
+      path      => '/etc/default/geodns',
+      ensure    => file,
+      require   => Exec['geodns-install'],
+      content   => template('geodns/geodns-defaults.erb')
+   }
+   service {'geodns':
+      ensure    => 'running',
+      require   => [File['geodns-upstart'],File['geodns-defaults']]
+   }
+   ufw::allow { "allow-dns":
+      ip   => 'any',
+      port => 53,
+      proto => "udp"
+   }
+}
